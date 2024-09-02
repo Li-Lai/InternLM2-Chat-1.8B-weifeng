acc5dbfc8db26fe44d34377e277b830073efec94
nuistmj001 <987491032@qq.com>
2024-09-02 18:23:08 +0800

    update code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
-download internlm2 to the base_path directory using git tool
+# download internlm2 to the base_path directory using git tool
 base_path = './internlm2_5-1_8b-chat'
 os.system(f'git clone https://oauth2:SvJo8x76g57C2Cog5joj@www.modelscope.cn/nuistmj001/InternLM2-Chat-1.8B-weifeng.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')

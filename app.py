e65b3cc4be79f53726f7233003a52b4a25edb75d
nuistmj001 <987491032@qq.com>
2024-09-02 18:06:15 +0800

    update code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
-base_path = './InternLM2-Chat-1.8B'
+base_path = './internlm2_5-1_8b-chat'
 os.system(f'git clone https://oauth2:SvJo8x76g57C2Cog5joj@www.modelscope.cn/nuistmj001/InternLM2-Chat-1.8B-weifeng.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
@@ -16,7 +16,7 @@ def chat(message,history):
         yield response
 
 gr.ChatInterface(chat,
-                 title="InternLM2-Chat-1.8B",
+                 title="internlm2_5-1_8b-chat",
                 description="""
 InternLM is mainly developed by Shanghai AI Laboratory.  
                  """,

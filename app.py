7a54f05ef9a1c4d4ccc2a074fb8a9d56574a2018
nuistmj001 <987491032@qq.com>
2024-09-02 18:21:22 +0800

    update code

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
-base_path = './internlm2_5-1_8b-chat'
-os.system(f'git clone https://oauth2:SvJo8x76g57C2Cog5joj@www.modelscope.cn/nuistmj001/InternLM2-Chat-1.8B-weifeng.git {base_path}')
-os.system(f'cd {base_path} && git lfs pull')
+# base_path = './internlm2_5-1_8b-chat'
+# os.system(f'git clone https://oauth2:SvJo8x76g57C2Cog5joj@www.modelscope.cn/nuistmj001/InternLM2-Chat-1.8B-weifeng.git {base_path}')
+# os.system(f'cd {base_path} && git lfs pull')
+
+base_path = '/root/InternLM/XTuner/internlm2_5-1_8b-chat'
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16)#.cuda()
